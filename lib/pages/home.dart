2515d2c2ad9335ada7b09494f6896f3f3ddcb46b
Alvaro RPL <keyza_faiq_33rpl@student.smktelkom-mlg.sch.id>
2025-11-19 20:04:43 +0700

    first commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -35,6 +35,84 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      drawer: Drawer(
+        width: 200,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.black,
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(width: 15),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios_new,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Menu",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            ListTile(
+              tileColor: const Color.fromARGB(255, 228, 228, 228),
+              iconColor: Colors.black,
+              textColor: Colors.black,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Icon(Icons.home_filled),
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              iconColor: Colors.black,
+              textColor: Colors.black,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Icon(Icons.favorite_border),
+              ),
+              title: Text("Wishlist"),
+              onTap: () {},
+            ),
+            ListTile(
+              iconColor: Colors.black,
+              textColor: Colors.black,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Icon(Icons.history),
+              ),
+              title: Text("History"),
+              onTap: () {},
+            ),
+            ListTile(
+              iconColor: Colors.black,
+              textColor: Colors.black,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Icon(Icons.support_agent),
+              ),
+              title: Text("Support"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+
       appBar: appBar(),
       body: ListView(children: [_searchBar(), _newDrops(), _inventory()]),
     );
@@ -386,14 +464,18 @@ class _HomePageState extends State<HomePage> {
       elevation: 0.1,
       backgroundColor: Colors.transparent,
       surfaceTintColor: Colors.transparent,
-      leading: Row(
-        children: [
-          SizedBox(width: 15),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.menu, color: Colors.black, size: 24),
-          ),
-        ],
+      leading: Builder(
+        builder: (context) {
+          return Row(
+            children: [
+              SizedBox(width: 15),
+              IconButton(
+                onPressed: () => Scaffold.of(context).openDrawer(),
+                icon: Icon(Icons.menu, color: Colors.black, size: 24),
+              ),
+            ],
+          );
+        },
       ),
 
       title: Text(
